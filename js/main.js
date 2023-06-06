d392a317767c905416b9dc4d964b3dd3de2a07b6
mohibkay <mohibk.in@gmail.com>
2023-06-06 23:55:49 +0530

    hide form, show elevators

--- js/main.js
+++ js/main.js
@@ -3,16 +3,31 @@ const liftInput = document.querySelector('.liftInput');
 const errorMsg = document.querySelector('.errorMsg');
 const simulateBtn = document.querySelector('.simulateBtn');
 
+function hideFormShowLift(shouldHide) {
+  const mainSection = document.querySelector('.mainSection');
+  const formContainer = document.querySelector('.formContainer');
+  mainSection.style.display = shouldHide ? 'block' : 'none';
+  formContainer.style.display = shouldHide ? 'none' : 'block';
+}
+
 function createFloorsAndLifts() {
-  console.log('running');
-  validateSubmission(floorInput.value, liftInput.value);
+  const floors = floorInput.value;
+  const lifts = liftInput.value;
+  if (!validateSubmission(floors, lifts)) return;
+  hideFormShowLift(true);
+}
+
+function calculateDistance(floor, lift) {
+  return Math.abs(floor - lift);
 }
 
 function validateSubmission(floor, lift) {
   errorMsg.textContent = null;
   if (floor <= lift) {
     errorMsg.textContent = 'Floors must be greater than elevators';
+    return false;
   }
+  return true;
 }
 
 simulateBtn.addEventListener('click', createFloorsAndLifts);
